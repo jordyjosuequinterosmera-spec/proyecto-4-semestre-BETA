c82668bfc121266e46bed4edeac3680afd8dca91
JordyCe <enmijordy68nl@gmail.com>
2026-02-08 22:34:36 -0500

    controlador vista Eventos

---
+++ src/infraestructura/Controladores/evento.controller.ts
@@ -0,0 +1,16 @@
+﻿import { Request, Response } from "express";
+import { EventoUseCase } from "../../application/evento.usecase";
+
+export class EventoController {
+    constructor(private eventoUseCase: EventoUseCase) {}
+
+    public getCtrl = async ({ res }: { res: Response }) => {
+        const eventos = await this.eventoUseCase.obtenerEventos();
+        res.send({ eventos });
+    };
+
+    public postReservaCtrl = async ({ body, res }: { body: any, res: Response }) => {
+        const reserva = await this.eventoUseCase.registrarReserva(body);
+        res.send({ reserva });
+    };
+}
