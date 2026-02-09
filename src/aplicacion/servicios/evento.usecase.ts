b8b1b62a3fceee475d5cc35c582f19fcf3bf8f41
JordyCe <enmijordy68nl@gmail.com>
2026-02-08 22:43:52 -0500

    agregar aplicación - vista de Eventos

---
+++ src/aplicacion/servicios/evento.usecase.ts
@@ -0,0 +1,15 @@
+﻿import { EventoRepository } from "../domain/evento.repository";
+import { Reserva } from "../domain/reserva.entity";
+
+export class EventoUseCase {
+    constructor(private readonly eventoRepository: EventoRepository) {}
+
+    public async obtenerEventos() {
+        const eventos = await this.eventoRepository.listarEventos();
+        return eventos;
+    }
+
+    public async registrarReserva(reserva: Reserva) {
+        return await this.eventoRepository.guardarReserva(reserva);
+    }
+}
