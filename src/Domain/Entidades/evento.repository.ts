41db30431b09ab7df21b40fa378b18dee5c44a1d
JordyCe <enmijordy68nl@gmail.com>
2026-02-08 22:39:40 -0500

    entidad vista de Eventos

---
+++ src/Domain/Entidades/evento.repository.ts
@@ -0,0 +1,8 @@
+﻿import { Evento } from "./evento.entity";
+import { Reserva } from "./reserva.entity";
+
+export interface EventoRepository {
+    listarEventos(): Promise<Evento[]>;
+    buscarEventoPorId(uuid: string): Promise<Evento | null>;
+    guardarReserva(reserva: Reserva): Promise<Reserva>;
+}
