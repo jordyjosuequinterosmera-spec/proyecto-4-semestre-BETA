61c5e1d77ae6063a3a494b25b1389ce3f0af42b2
JordyCe <enmijordy68nl@gmail.com>
2026-02-08 22:40:17 -0500

    entidad vista de Eventos

---
+++ src/Domain/Entidades/reserva.entity.ts
@@ -0,0 +1,11 @@
+﻿export class Reserva {
+    constructor(
+        public uuid: string,
+        public eventoId: string,
+        public nombreCliente: string,
+        public email: string,
+        public telefono: string,
+        public fechaEvento: Date,
+        public cantidadPersonas: number
+    ) {}
+}
