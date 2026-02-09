76f1f6ff36e105b9c65d9d47a178f33c1b88a318
JordyCe <enmijordy68nl@gmail.com>
2026-02-08 22:38:30 -0500

    entidad de vista Eventos

---
+++ src/Domain/Entidades/evento.entity.ts
@@ -0,0 +1,10 @@
+﻿export class Evento {
+    constructor(
+        public uuid: string,
+        public titulo: string,
+        public descripcion: string,
+        public precio: number,
+        public capacidad: string,
+        public imagenUrl: string
+    ) {}
+}
